6ecf3c3cc398752174f70920b24ea16990520ec7
kirteshshirodkar <kirteshshirodkar8@gmail.com>
2024-11-13 19:42:34 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'body': [
+        'Inter', 
+        'ui-sans-serif', 
+        'system-ui',
+        // other fallback fonts
+      ],
+      'sans': [
+        'Inter', 
+        'ui-sans-serif', 
+        'system-ui',
+        // other fallback fonts
+      ]
+  },
+  plugins: [],
+}
+}
